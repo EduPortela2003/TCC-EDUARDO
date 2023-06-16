69410bcc33c402c8082292efa606d8c353bb1a3a
EduPortela2003 <edu_portela@outlook.com>
2023-06-16 16:37:53 -0300

    TCC

---
+++ tcc/resources/views/layouts/main.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+        <!-- Fonte do Google -->
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+
+        <!-- CSS Bootstrap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+        <!-- CSS da aplicação -->
+        <link rel="stylesheet" href="/css/styles.css">
+        <script src="/js/scripts.js"></script>
+    </head>
+    <body>
+      <header>
+      <nav class="navbar navbar-expand-md navbar-dark fixed bg-dark">
+      <a class="navbar-brand" href="#">EventSphere</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item ">
+            <a class="nav-link " href="/">Eventos<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="/events/create">Novo evento<span class="sr-only">(current)</span></a>
+          </li>
+          @auth
+          <li class="nav-item ">
+            <a class="nav-link" href="/dashboard">Meus eventos<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+                <form action="/logout" method="POST">
+                  @csrf
+                  <a href="/logout" 
+                    class="nav-link" 
+                    onclick="event.preventDefault();
+                    this.closest('form').submit();">
+                    Sair
+                  </a>
+                </form>
+              </li>
+              @endauth
+              @guest
+              <li class="nav-item">
+                <a href="/login" class="nav-link">Entrar</a>
+              </li>
+              <li class="nav-item">
+                <a href="/register" class="nav-link">Cadastrar</a>
+              </li>
+              @endguest
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <div class="container-fluid">
+          <div class="row">
+            @if(session('msg'))
+              <p class="msg">{{ session('msg') }}</p>
+            @endif
+            @yield('content')
+          </div>
+        </div>
+      </main>
+      <footer>
+        <p>EventSphere &copy; 2023</p>
+      </footer>
+      <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
+    </body>
+</html>
